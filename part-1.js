523746e50e19400aa14f937e1c0dbee37fa286d7
RafM <rkjmosiolek@gmail.com>
2018-10-26 11:21:27 +0100

    read files, reverse them, saved reverse, saved reversed strings as separate files

--- part-1.js
+++ part-1.js
@@ -1,1 +1,12 @@
-console.log('your code here')
+var fs = require('fs');
+
+var fileContents = fs.readFileSync('names.txt', 'utf-8');
+var arrayOfLines = fileContents.split('\n');
+var reversedArrayOfLines = arrayOfLines.reverse();
+var joinedReversedString = reversedArrayOfLines.join('\n');
+
+fs.writeFileSync('names-reversed.txt', joinedReversedString, 'UTF-8');
+
+for (let i = 0; i < arrayOfLines.length; i++) {
+    fs.writeFileSync(arrayOfLines[i] + '-reversed.txt', arrayOfLines[i].split('').reverse().join(''), 'UTF-8');
+  }
