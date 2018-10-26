0ce6981423ae4c91e2d480ec360455f972777e1b
RafM <rkjmosiolek@gmail.com>
2018-10-26 11:42:04 +0100

    read data from json file, manipulate it,save it to the new file. complete part-2

--- part-2.js
+++ part-2.js
@@ -1,1 +1,19 @@
-console.log('your code here')
+var fs = require('fs');
+
+var fileContents = fs.readFileSync('shopping-basket.json', 'utf-8');
+var data = JSON.parse(fileContents);
+console.log(data);
+
+var totalPrice = 0;
+
+for (var item of data.basket) {
+    if (item.name === 'Candles') {
+        item.quantity = 10;
+        console.log(item.quantity);
+        var jsonString = JSON.stringify(data, null, 2)
+        fs.writeFileSync('new-shopping-basket.json', jsonString, 'UTF-8');
+    }
+    totalPrice +=item.price;
+  }
+
+console.log('the total basket price is: ' + totalPrice);
